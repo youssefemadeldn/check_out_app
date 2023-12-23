5099845ebc66cef7b3c8255b0d49495457d29c8b
youssef emad eldin <youssefemadeldn>
2023-12-23 10:41:39 +0200

    create appBar part 2: add svg 2

--- lib/Features/checkout/presentation/views/my_cart_view.dart
+++ lib/Features/checkout/presentation/views/my_cart_view.dart
@@ -1,5 +1,6 @@
 import 'package:check_out_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MyCartView extends StatelessWidget {
   const MyCartView({super.key});
@@ -8,6 +9,8 @@ class MyCartView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: Center(child: SvgPicture.asset('assets/images/arrow.svg')),
+        centerTitle: true,
         title: const Text(
           'My Cart',
           textAlign: TextAlign.center,
