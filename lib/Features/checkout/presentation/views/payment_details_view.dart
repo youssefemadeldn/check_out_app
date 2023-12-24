d0474473c99c53f428ad6fe26178e6c5294fa576
youssef emad eldin <youssefemadeldn>
2023-12-24 16:46:26 +0200

    create payment item: done 1

--- lib/Features/checkout/presentation/views/payment_details_view.dart
+++ lib/Features/checkout/presentation/views/payment_details_view.dart
@@ -1,4 +1,5 @@
 import 'package:check_out_app/core/widgets/custom_app_bar_widget.dart';
+import 'package:check_out_app/core/widgets/payment_details_view_body.dart';
 import 'package:flutter/material.dart';
 
 class PaymentDetailsView extends StatelessWidget {
@@ -8,6 +9,7 @@ class PaymentDetailsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(title: 'Payment Details'),
+      body: const PaymentDetailsViewBody(),
     );
   }
 }
---
+++ lib/core/widgets/payment_details_view_body.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class PaymentDetailsViewBody extends StatelessWidget {
+  const PaymentDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 103,
+          height: 62,
+          decoration: ShapeDecoration(
+            shape: RoundedRectangleBorder(
+              side: const BorderSide(width: 1.5, color: Color(0xFF34A853)),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            shadows: const [
+              BoxShadow(
+                color: Color(0xFF34A853),
+                blurRadius: 4,
+                // offset: Offset(0, 0),
+                // spreadRadius: 0,
+              )
+            ],
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(15)),
+            child: Center(
+              child: SvgPicture.asset(
+                'assets/images/master_card.svg',
+                // height: 24,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
