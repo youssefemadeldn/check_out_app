50653724d2069bc4bf0a06eea6b50143c9e95dec
youssef emad eldin <youssefemadeldn>
2023-12-28 13:56:14 +0200

    flutter credit card part tow: CreditCardForm Widget, formKey, 2

--- lib/core/widgets/payment_details_view_body.dart
+++ lib/core/widgets/payment_details_view_body.dart
@@ -26,6 +26,7 @@ class CustomCreditCard extends StatefulWidget {
 class _CustomCreditCardState extends State<CustomCreditCard> {
   // must initialize those variables
   String cardNumber = '', expiryDate = '', cardHolderName = '', cvvCode = '';
+  final GlobalKey<FormState> formKey = GlobalKey();
 
   bool showBackView = false;
 
