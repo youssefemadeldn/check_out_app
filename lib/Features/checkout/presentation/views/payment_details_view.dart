2863aacd4cd01abbc18771c4d57a245df3698ff8
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-12-28 17:00:10 +0200

    Edit on PaymentDetailsView file

--- lib/Features/checkout/presentation/views/payment_details_view.dart
+++ lib/Features/checkout/presentation/views/payment_details_view.dart
@@ -9,7 +9,9 @@ class PaymentDetailsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(title: 'Payment Details'),
-      body: const SingleChildScrollView(child: PaymentDetailsViewBody()),
+      body: const SingleChildScrollView(
+        child: PaymentDetailsViewBody(),
+      ),
     );
   }
 }
