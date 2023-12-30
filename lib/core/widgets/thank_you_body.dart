fec28be0471651fa316c0cdb6ad348c0aabec4c2
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-12-30 09:49:26 +0200

    23. Custom shaped container: done 1

--- lib/core/widgets/thank_you_body.dart
+++ lib/core/widgets/thank_you_body.dart
@@ -5,6 +5,34 @@ class ThankYouBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: Stack(
+        children: [
+          Container(
+            decoration: ShapeDecoration(
+              color: const Color(0xFFD9D9D9),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: MediaQuery.sizeOf(context).height * .2,
+            left: -20,
+            child: const CircleAvatar(
+              backgroundColor: Colors.white,
+            ),
+          ),
+          Positioned(
+            bottom: MediaQuery.sizeOf(context).height * .2,
+            right: -20,
+            child: const CircleAvatar(
+              backgroundColor: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
