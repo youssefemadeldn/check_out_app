c2068c7faa8056ed67b2f6fd818be79733cc5b4d
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-12-30 16:23:06 +0200

    edit on ThankYouBody buildAppBar

--- lib/core/widgets/custom_app_bar_widget.dart
+++ lib/core/widgets/custom_app_bar_widget.dart
@@ -2,12 +2,12 @@ import 'package:check_out_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-AppBar buildAppBar({required final String title}) {
+AppBar buildAppBar({final String? title}) {
   return AppBar(
     leading: Center(child: SvgPicture.asset('assets/images/arrow.svg')),
     centerTitle: true,
     title: Text(
-      title,
+      title ?? '',
       textAlign: TextAlign.center,
       style: Styles.style25,
     ),
