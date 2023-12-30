1582b219749cf39f91783194c2add35189532653
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-12-30 14:21:19 +0200

    27. Create payment item info: Done 1

--- lib/core/widgets/background_of_thank_you_card.dart
+++ lib/core/widgets/background_of_thank_you_card.dart
@@ -1,3 +1,4 @@
+import 'package:check_out_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class BackgroundOfThankYouCard extends StatelessWidget {
@@ -8,12 +9,77 @@ class BackgroundOfThankYouCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity,
       decoration: ShapeDecoration(
         color: const Color(0xFFD9D9D9),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
       ),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 50 + 26, left: 23, right: 23),
+        child: Column(
+          children: [
+            const Text(
+              'Thank You!',
+              style: Styles.style25,
+            ),
+            Text(
+              'Your transaction was successful',
+              textAlign: TextAlign.center,
+              style: Styles.style20,
+            ),
+            const SizedBox(
+              height: 42,
+            ),
+            const PaymentItemInfo(
+              title: 'Date',
+              value: '01/24/2023',
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const PaymentItemInfo(
+              title: 'Time',
+              value: '10:15 AM',
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const PaymentItemInfo(
+              title: 'To',
+              value: 'Sam Louis',
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PaymentItemInfo extends StatelessWidget {
+  final String title, value;
+  const PaymentItemInfo({super.key, required this.title, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          textAlign: TextAlign.center,
+          style: Styles.style18,
+        ),
+        Text(
+          value,
+          textAlign: TextAlign.center,
+          style: Styles.styleBold18,
+        ),
+      ],
     );
   }
 }
