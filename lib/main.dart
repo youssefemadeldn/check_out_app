fb9eae9666a75842332f6e5ab4a426a40c6e67a0
youssef emad eldin <youssefemad63.ye@gmail.com>
2024-02-24 17:21:47 +0200

    10. Init payment sheet: Video Done

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,25 @@ class CheckOutApp extends StatelessWidget {
 
 
         /*
-         Main Steps:
-  1-  Create account => https://dashboard.stripe.com/register
-  2- create payment intent (amount , currency)
+
+    Main Steps:
+    1. Set up Stripe [Server Side] [Client Side]
+      1-  Create account => https://dashboard.stripe.com/register [Server Side] [Client Side]
+    
+    2. Add an endpoint [Server Side]
+      2- create payment intent (amount , currency)
+      
+    3. Integrate the payment sheet [Client Side]
+      3-  initPaymentSheet ( paymentIntentClientSecret )
+
+
+
+                  Summary:
+  1- Create account => https://dashboard.stripe.com/register 
+  2- paymentIntentObjectAsReturnType createPaymentIntentMethod ( amount , currency )
+  3- initPaymentSheet ( paymentIntentClientSecret ) 
 
         */
+
+
+
