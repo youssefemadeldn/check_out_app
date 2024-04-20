ccdda5ebc012c4dd178fee9dbf8d125b67a2231d
youssef emad eldin <youssefemad63.ye@gmail.com>
2024-04-20 13:40:12 +0200

    22. Create checkout cubit: create Cubit 4️⃣

--- lib/Features/checkout/presentation/manager/cubit/stripe_payment_cubit.dart
+++ lib/Features/checkout/presentation/manager/cubit/stripe_payment_cubit.dart
@@ -1,8 +1,32 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
+import 'package:check_out_app/Features/checkout/data/models/payment_intent_input_model.dart';
+import 'package:check_out_app/Features/checkout/data/repos/checkout_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'stripe_payment_state.dart';
 
 class StripePaymentCubit extends Cubit<StripePaymentState> {
-  StripePaymentCubit() : super(StripePaymentInitial());
+  final CheckoutRepo checkoutRepo;
+  StripePaymentCubit(this.checkoutRepo) : super(StripePaymentInitial());
+
+  Future mackPayment(
+      {required PaymentIntentInputModel paymentIntentInputModel}) async {
+    emit(StripePaymentLoading());
+
+    var data = await checkoutRepo.makePayment(
+        paymentIntentInputModel: paymentIntentInputModel);
+
+    data.fold(
+      (l) => emit(StripePaymentFailure(l.errMessage)),
+      (r) => emit(StripePaymentSuccess()),
+    );
+  }
+
+  @override
+  void onChange(Change<StripePaymentState> change) {
+    log(Change as String);
+    super.onChange(change);
+  }
 }
