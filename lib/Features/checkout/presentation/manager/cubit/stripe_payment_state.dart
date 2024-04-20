fe59bdcb75c93de5e4de477f7a051b20701b61e8
youssef emad eldin <youssefemad63.ye@gmail.com>
2024-04-20 13:26:39 +0200

    22. Create checkout cubit: create states 3️⃣

--- lib/Features/checkout/presentation/manager/cubit/stripe_payment_state.dart
+++ lib/Features/checkout/presentation/manager/cubit/stripe_payment_state.dart
@@ -4,3 +4,13 @@ part of 'stripe_payment_cubit.dart';
 sealed class StripePaymentState {}
 
 final class StripePaymentInitial extends StripePaymentState {}
+
+final class StripePaymentLoading extends StripePaymentState {}
+
+final class StripePaymentSuccess extends StripePaymentState {}
+
+final class StripePaymentFailure extends StripePaymentState {
+  final String errMessage;
+
+  StripePaymentFailure(this.errMessage);
+}
